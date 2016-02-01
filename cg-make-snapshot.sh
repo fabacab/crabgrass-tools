607d2c432c358e88fece1e2c1d213e150fb2cb28
Meitar Moscovitz <meitarm@gmail.com>
2016-02-01 10:34:01 -0700

    Add option for mirroring subgroups, allow non-interactive operation.

--- cg-make-snapshot.sh
+++ cg-make-snapshot.sh
@@ -37,30 +37,73 @@
 # Cleanup on exit, or when interrupted for any reason.
 trap 'cleanup' QUIT EXIT
 
-# Constants
+# For security reasons, explicitly set the internal field separator
+# to newline, space, tab
+readonly OLD_IFS=$IFS
+IFS='
+ 	'
+
+# CONSTANTS
 readonly PROGRAM=`basename "$0"`
+readonly VERSION="0.0.2"
 readonly COOKIE_FILE=`mktemp ${PROGRAM}_cookies.XXXXX`
 readonly LOGIN_PATH="/session/login"
 
-# TODO: This should be user-settable.
-readonly BASE_URL="https://we.riseup.net"
+# RETURN VALUES/EXIT STATUS CODES
+readonly E_MISSING_ARG=253
+readonly E_BAD_OPTION=254
+readonly E_UNKNOWN=255
 
-CG_GROUP="$1"
+# DEFAULT ARGUMENTS
+DEFAULT_BASE_URL="https://we.riseup.net"
 
-# Function: login
+# ARGUMENTS
+BASE_URL=""
+USERNAME=""
+PASSWORD=""
+DRY_RUN=""
+SUBGROUP=0
+
+# Function: version
 #
-# Logs in to the Crabgrass server.
+# Uses globals:
+#     $PROGRAM
+#     $VERSION
+function version () {
+    echo "$PROGRAM version $VERSION"
+}
+
+# Function: usage
 #
-# Will create a cookies file for later use in $COOKIE_FILE
-#
-# $1 username
-# $2 password
-# $3 login_url
-function login () {
-    local post_data="login=${1}&password=${2}"
-    wget --save-cookies "$COOKIE_FILE" --keep-session-cookies \
-        --post-data "$post_data" -O /dev/null \
-            "$3"
+# Explains command line arguments.
+function usage () {
+    echo "Usage is as follows:"
+    echo
+    echo "$PROGRAM <--version>"
+    echo "    Prints the program version number on a line by itself and exits."
+    echo
+    echo "$PROGRAM <--usage|--help|-?>"
+    echo "    Prints this usage output and exits."
+    echo
+    echo "$PROGRAM [--username|--user|-u <user>] [--password|-p <password>]"
+    echo "         [--subgroup] [--base-url] [--dry-run] <cg_group> [cg_group_2 ...]"
+    echo "    Creates a static backup (an HTML snapshot) of <cg_group>."
+    echo "    You can pass additional group names to snapshot them, as well."
+    echo
+    echo "    If '--user' is specified, does not prompt for a Crabgrass username."
+    echo
+    echo "    If '--password' is specified, does not prompt for a Crabgrass password."
+    echo "    Note that passing password arguments on the command line is insecure and"
+    echo "    may reveal your password to users on the system where $PROGRAM runs."
+    echo
+    echo "    If '--subgroup' is specified, looks for committees and/or councils that"
+    echo "    are associated with <crabgrass_group> and includes them in the snapshot."
+    echo
+    echo "    If '--dry-run' is specified, $PROGRAM echos the mirroring commands that"
+    echo "    would have been run, but does not actually create a snapshot."
+    echo
+    echo "    If '--base-url' is specified, uses the Crabgrass instance running at"
+    echo "    the given URL. Defaults to: $DEFAULT_BASE_URL"
 }
 
 # Function: cleanup
@@ -72,20 +115,145 @@ function login () {
 # Uses global $COOKIE_FILE
 function cleanup () {
     rm -f $COOKIE_FILE
+    IFS="$OLD_IFS"
 }
 
-# Get Crabgrass parameters.
-# (The crabgrass group name is passed to the script in $1.)
-# TODO: Provide a non-interactive option so users can automate this.
-echo -n "Username: "
-read USERNAME
-echo -n "Password: "
-read -s PASSWORD
+# Function: login
+#
+# Logs in to the Crabgrass server.
+#
+# Will create a cookies file for later use in $COOKIE_FILE
+function login () {
+    local username="$1"
+    local password="$2"
+    local login_url="$3"
 
-login "$USERNAME" "$PASSWORD" "${BASE_URL}${LOGIN_PATH}"
+    local post_data="login=${username}&password=${password}"
+    echo
+    echo "Logging in as $USERNAME..."
+    wget --quiet --save-cookies "$COOKIE_FILE" --keep-session-cookies \
+        --post-data "$post_data" -O /dev/null \
+            "$login_url"
+}
 
-INCLUDE_PATH="/${CG_GROUP},/groups/${CG_GROUP}"
+# Function: getSubgroups
+#
+# Finds the URL paths of the subgroups of a given group.
+#
+# Outputs each found group on its own line.
+function getSubgroups () {
+    local group="$1"
+    wget --quiet --load-cookies "$COOKIE_FILE" -O - "${BASE_URL}/${group}/" \
+        | grep "href=\"/${group}+" \
+            | cut -d '"' -f 2 | cut -d '/' -f 2 \
+                | grep "$group" | uniq
+}
 
-# Download homepage.
-wget --load-cookies "$COOKIE_FILE" --mirror --include "$INCLUDE_PATH" --convert-links \
-    --retry-connrefused --page-requisites --html-extension "${BASE_URL}/${CG_GROUP}/"
+# Function: mirrorGroup
+#
+# Downloads (mirrors) the given group.
+#
+# Uses globals:
+#     $DRY_RUN
+#     $COOKIE_FILE
+#     $BASE_URL
+function mirrorGroup () {
+    local group="$1"
+    local include_path="/${group},/groups/${group}"
+    echo "Mirroring group $group..."
+    $DRY_RUN wget --load-cookies "$COOKIE_FILE" --mirror \
+        --include "$include_path" --convert-links --retry-connrefused \
+        --page-requisites --html-extension "${BASE_URL}/${group}/"
+}
+
+function main () {
+    # Process command line arguments.
+    while test $# -gt 0; do
+        if [ x"$1" == x"--" ]; then
+            # detect argument termination
+            shift
+            break
+        fi
+        case $1 in
+            --username | --user | -u )
+                shift
+                USERNAME="$1"
+                shift
+                ;;
+
+            --password | -p )
+                shift
+                PASSWORD="$1"
+                shift
+                ;;
+
+            --subgroup )
+                shift
+                SUBGROUP=1
+                ;;
+
+            --dry-run )
+                shift
+                DRY_RUN="echo"
+                ;;
+
+            --base-url )
+                shift
+                BASE_URL="$1"
+                shift
+                ;;
+
+            --version )
+                version
+                exit
+                ;;
+
+            -? | --usage | --help )
+                usage
+                exit
+                ;;
+
+            -* )
+                echo "Unrecognized option: $1" >&2
+                usage
+                exit $E_BAD_OPTION
+                ;;
+
+            * )
+                break
+                ;;
+        esac
+    done
+
+    # We still need group names.
+    if [ $# -lt 1 ]; then
+        usage
+        exit $E_MISSING_ARG
+    fi
+
+    # Get Crabgrass parameters.
+    if [ -z "$USERNAME" ]; then
+        read -p "Username: " USERNAME
+    fi
+    if [ -z "$PASSWORD" ]; then
+        read -s -p "Password: " PASSWORD
+    fi
+    if [ -z "$BASE_URL" ]; then
+        BASE_URL="$DEFAULT_BASE_URL"
+    fi
+
+    login "$USERNAME" "$PASSWORD" "${BASE_URL}${LOGIN_PATH}"
+
+    for group in "$@"; do
+        mirrorGroup "$group"
+        if [ 1 -eq $SUBGROUP ]; then
+            echo "Finding subgroups for $group..."
+            read -r -a subgroups <<< `getSubgroups "$group"`
+            for subgroup in ${subgroups[@]}; do
+                mirrorGroup "$subgroup"
+            done
+        fi
+    done
+}
+
+main "$@"

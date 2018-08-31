bd33243e9117010b63ac4c5042433587c5455625
Meitar M <meitarm@gmail.com>
2018-08-31 17:54:11 -0400

    Add support for commiting to Git repositories automatically.
    This commit adds a number of new command-line options. Most notable of
    these is the ability to pass `--scm`, which enables the source code
    management feature. This feature automatically looks for a SCM checkout
    at the location of the `--download-directory` and commits the newly
    downloaded content as a revision atop the existing repository. If a
    repository does not exist, it will be automatically intialized.
    
    The only SCM backend currently supported is Git, so passing `--scm` and
    `--scm git` are equivalent.
    
    The `--scm-args` option can be given a single (quoted) argument that
    will be passed to the SCM backend chosen. This allows for (almost)
    arbitrarily complex commit options to be provided and should make
    integration with other SCM systems easier in the future, too. Beware
    that parsing multiple options from this single string is currently
    somewhat naive.
    
    This commit further adds support for arbitrary `wget` options using a
    similar `--wget-args` option.
    
    Documentation has been updated, as well, with plenty more code comments
    and a number of additional examples in the README and `usage()` output.

--- README.markdown
+++ README.markdown
@@ -10,10 +10,17 @@ This repository holds various scripts and tools that make working with [Crabgras
 
 * find and mirror subgroups ("committees" and "councils")
 * mirror any Crabgrass instance (not just the one at `we.riseup.net`)
+* restrict itself to making network requests only over the [Tor](https://torproject.org/) anonymizing network (and, when combined with a custom `--base-url`, can connect to Onion sites hosting a Crabgrass instance)
+* automatically commit snapshot changes to a source code management repository (like Git)
 
 ### Dependencies
 
+The following should be in your search `$PATH`:
+
 * `wget`
+* If you choose to enable the `--scm` features, you further need:
+    * the SCM binary of your choice (e.g., `git`), and
+    * `sed`, for parsing complex arguments passed through to the underlying SCM backend.
 
 ### Example uses
 
@@ -38,8 +45,20 @@ Make a snapshot of the Crabgrass group `my-group` and all its committees' conten
 Make a snapshot of the Crabgrass group `my-other-group` and its subgroups when `my-other-group` lives on a personal Crabgrass server located at `cg.example.com`:
 
 ```sh
-./cg-make-snapshot --base-url https://cg.example.com --subgroup my-other-group
+./cg-make-snapshot.sh --base-url https://cg.example.com --subgroup my-other-group
 ```
+
+Make a snapsot of the Crabgrass group `frequently-updated-group` and its subgroups and save only the changes since the last snapshot into a Git repository located at `/opt/local/var/backup/crabgrass` that is committed to as the author known as `"A U Thor <a@thor.com>"`, and do this all in an unattended fashion (by specifying the user `backups` on the command line and reading the password from the `~/.crabgrass.secret` file on disk), asking `wget` to be `--quiet` during operation, while only making network requests using Tor:
+
+```sh
+./cg-make-snapshot.sh --download-directory /opt/local/var/backup/crabgrass \
+    --user backups --password "$(cat $HOME/.crabgrass.secret)" \
+    --subgroup --tor --quiet \
+    --scm --scm-args '--author="A U Thor <a@thor.com>" -m "Automated backup."' \
+    frequently-updated-group >/dev/null 2>&1
+```
+
+This last example is suitable for use in a `cron` script, for example, to automate your Crabgrass backups, as it produces zero output (by redirecting `stdout` and `stderr` to `/dev/null`).
 
 ### Known issues
 
--- cg-make-snapshot.sh
+++ cg-make-snapshot.sh
@@ -3,7 +3,8 @@
 # File:        cg-make-snapshot.sh
 #
 # Description: A utility script that takes a static snapshot of any
-#              Crabgrass group that you have access to.
+#              Crabgrass group that you have access to and optionally
+#              commits the changes to a given Git repository.
 #
 # Examples:    Use cg-make-snapshot.sh to create a mirror containing
 #              the contents of a Crabgrass group.
@@ -43,52 +44,78 @@ readonly OLD_IFS=$IFS
 IFS=$'\n \t'
 
 # CONSTANTS
-readonly PROGRAM=`basename "$0"`
-readonly VERSION="0.0.3"
-readonly COOKIE_FILE=`mktemp ${PROGRAM}_cookies.XXXXX`
+# TODO: These should be lower-cased so as not to conflict with
+#       with any pre-existing environment variables.
+readonly PROGRAM=$(basename "$0")
+readonly VERSION="0.1.0"
+readonly WGET="$(which wget)"
+readonly COOKIE_FILE=$(mktemp "${PROGRAM}_cookies.XXXXX")
 readonly LOGIN_PATH="/session/login"
 
 # RETURN VALUES/EXIT STATUS CODES
+readonly E_NONWRITABLE_DIR=250
+readonly E_MISSING_SCM=251
+readonly E_MISSING_WGET=252
 readonly E_MISSING_ARG=253
 readonly E_BAD_OPTION=254
 readonly E_UNKNOWN=255
 
 # DEFAULT ARGUMENTS
-DEFAULT_DOWNLOAD_DIR="."
 DEFAULT_BASE_URL="https://we.riseup.net"
+DEFAULT_DOWNLOAD_DIR="."
+DEFAULT_SCM="git"
 
 # ARGUMENTS
+# The base URL of the Crabgrass instance.
 BASE_URL=""
+# The username with which to log in to Crabgrass.
 USERNAME=""
+# The password with which to log in to Crabgrass.
 PASSWORD=""
+# Whether to merely echo mirroring commands or actually perform them.
 DRY_RUN=""
+# Custom arguments to pass to the `wget` invocation.
+WGET_ARGS=""
+# Whether to use `torsocks(1)` to proxy all requests via Tor.
 TOR=""
+# Whether to mirror Crabgrass subgroups ("committees") as well.
 SUBGROUP=0
+# Whether or not to enable the SCM features.
+USE_SCM=0
+# Which source code management system to use, if any.
+SCM=""
+# Arguments to pass to the chosen SCM backend, if any.
+SCM_ARGS=""
 
 # Function: version
 #
 # Uses globals:
 #     $PROGRAM
 #     $VERSION
-function version () {
+#     $WGET
+version () {
     echo "$PROGRAM version $VERSION"
+    echo
+    echo "Using wget at $WGET"
 }
 
 # Function: usage
 #
 # Explains command line arguments.
-function usage () {
+usage () {
     echo "Usage is as follows:"
     echo
     echo "$PROGRAM <--version>"
-    echo "    Prints the program version number on a line by itself and exits."
+    echo "    Prints the program version number on a line by itself, along with"
+    echo "    details about dependent programs, then exits."
     echo
     echo "$PROGRAM <--usage|--help|-?>"
     echo "    Prints this usage output and exits."
     echo
     echo "$PROGRAM [--username|--user|-u <user>] [--password|-p <password>]"
-    echo "         [--download-directory <dir>]"
+    echo "         [--wget-args <wget_args>] [--download-directory <dir>] [--quiet|-q]"
     echo "         [--base-url <url>] [--subgroup] [--dry-run] [--tor]"
+    echo "         [--scm [git] [--scm-args <scm_args>]]"
     echo "         <crabgrass_group> [crabgrass_group_2 [crabgrass_group_N...]]"
     echo "    Creates a static backup (an HTML snapshot) of <cg_group>."
     echo "    You can pass additional group names to snapshot them, as well."
@@ -99,9 +126,16 @@ function usage () {
     echo "    Note that passing password arguments on the command line is insecure and"
     echo "    may reveal your password to users on the system where $PROGRAM runs."
     echo
+    echo "    If '--wget-args' is specified, the value is passed directly to 'wget' as"
+    echo "    a command-line option, allowing for (almost) arbitrary 'wget' invocations."
+    echo
     echo "    If '--download-directory' is specified, all files will be downloaded to"
-    echo "    the given directory. Defaults to the current working directory,"
-    echo "    which is: $DEFAULT_DOWNLOAD_DIR"
+    echo "    the given directory. Defaults to the current working directory ('.'),"
+    echo "    and is effectively a shortcut for '--wget-args \"--directory-prefix\"'."
+    echo
+    echo "    If '--quiet' or '-q' is specified, this argument is passed to wget at"
+    echo "    runtime. By default, wget output is very loud. This is a shortcut for"
+    echo "    using '--wget-args \"--quiet\", for example.'"
     echo
     echo "    If '--base-url' is specified, uses the Crabgrass instance running at"
     echo "    the given URL. Defaults to: $DEFAULT_BASE_URL"
@@ -114,6 +148,42 @@ function usage () {
     echo
     echo "    If '--tor' is specified, $PROGRAM uses torsocks(1) to proxy all requests"
     echo "    through Tor."
+    echo
+    echo "    If '--scm' is specified, $PROGRAM will commit the mirror to a source code"
+    echo "    version control system. If you use '--scm' you can also use '--scm-args'"
+    echo "    to supply additional command line arguments to the chosen SCM backend."
+    echo "    Supported SCM backends are: git (default)"
+    echo "    If there is interest, I'll consider adding 'cvs', 'svn', 'hg', and 'bzr'."
+    echo
+    echo "EXAMPLES"
+    echo
+    echo "Use $PROGRAM to create a mirror containing the contents of"
+    echo "the Crabgrass group 'examplegroup'. The username and password"
+    echo "with which to log in will be requested interactively. The mirror"
+    echo "will be created in the current directory."
+    echo
+    echo "    $PROGRAM examplegroup"
+    echo
+    echo
+    echo "Mirror 'othergroup' and all its committees, but perform all"
+    echo "network requests by traveling over Tor, and don't ask for a"
+    echo "username interactively (use the account 'myname'). Still do"
+    echo "ask for the password interactively so that it's not exposed"
+    echo "via the command line)."
+    echo
+    echo "    $PROGRAM --tor --subgroup --user myname othergroup"
+    echo
+    echo
+    echo "Mirror 'othergroup' and its committees by downloading files"
+    echo "into the /opt/local/var/backup/crabgrass directory and then"
+    echo "either create a Git repository at that location or commit"
+    echo "the newly downloaded mirror as a new commit to the existing"
+    echo "Git repository already storing prior backups there."
+    echo
+    echo "    $PROGRAM  --subgroup --scm \\"
+    echo "        --scm-args '--author=\"A U Thor <a@thor.com>\"' \\"
+    echo "        --download-directory /opt/local/var/backup/crabgrass \\"
+    echo "        othergroup"
 }
 
 # Function: cleanup
@@ -122,8 +192,10 @@ function usage () {
 # This is important because the cookie is an authentication token and
 # if it is compromised an attacker could impersonate a legitmate user.
 #
-# Uses global $COOKIE_FILE
-function cleanup () {
+# Uses globals:
+#     $COOKIE_FILE
+#     $OLD_IFS
+cleanup () {
     rm -f $COOKIE_FILE
     IFS="$OLD_IFS"
 }
@@ -132,7 +204,7 @@ function cleanup () {
 #
 # URL-encodes (percent-encodes) the given value.
 # This is needed to properly send complex passwords.
-function urlencode () {
+urlencode () {
     local string="$1"
     echo -n "$(echo -n "$string" | \
         while IFS='' read -r -d '' -n 1 c; do \
@@ -146,7 +218,13 @@ function urlencode () {
 # Logs in to the Crabgrass server.
 #
 # Will create a cookies file for later use in $COOKIE_FILE
-function login () {
+#
+# Uses globals:
+#    $TOR
+#    $WGET
+#    $COOKIE_FILE
+#    $BASE_URL
+login () {
     local username="$1"
     local password="$2"
     local login_url="$3"
@@ -156,7 +234,7 @@ function login () {
     local post_data  # HTTP POST'ed data
 
     # Load remote site, get needed HTML, parse for token.
-    html="$($TOR wget --save-cookies "$COOKIE_FILE" --keep-session-cookies -O - "$BASE_URL" 2>/dev/null | \
+    html="$($TOR "$WGET" --save-cookies "$COOKIE_FILE" --keep-session-cookies -O - "$BASE_URL" 2>/dev/null | \
         grep -siE '(meta\s+name="csrf-param"|input\s+type="hidden")'
     )"
     csrf_param="$(echo "$html" | \
@@ -171,7 +249,7 @@ function login () {
     post_data="${csrf_param}=$(urlencode "$token")&login=${username}&password=$(urlencode "$password")"
     
     echo "Logging in as $USERNAME..."
-    $TOR wget --quiet --load-cookies "$COOKIE_FILE" --save-cookies "$COOKIE_FILE" --keep-session-cookies \
+    $TOR "$WGET" --quiet --load-cookies "$COOKIE_FILE" --save-cookies "$COOKIE_FILE" --keep-session-cookies \
         --post-data "$post_data" -O /dev/null \
             "$login_url"
 }
@@ -181,9 +259,9 @@ function login () {
 # Finds the URL paths of the subgroups of a given group.
 #
 # Outputs each found group on its own line.
-function getSubgroups () {
+getSubgroups () {
     local group="$1"
-    $TOR wget --quiet --load-cookies "$COOKIE_FILE" -O - "${BASE_URL}/${group}/" \
+    $TOR "$WGET" --quiet --load-cookies "$COOKIE_FILE" -O - "${BASE_URL}/${group}/" \
         | grep -E --only-matching "href=\"/${group}\+[^\"]+" \
             | cut -d '"' -f 2 | cut -d '/' -f 2 \
                 | grep "$group" | sort | uniq
@@ -198,14 +276,15 @@ function getSubgroups () {
 #     $COOKIE_FILE
 #     $DOWNLOAD_DIR
 #     $BASE_URL
-function mirrorGroup () {
+#     $SUBGROUP
+mirrorGroup () {
     local group="$1"
     local include_path="/${group},/groups/${group},/assets"
     local url="${BASE_URL}/${group}/"
 
     if [ 1 -eq $SUBGROUP ]; then
         echo "Finding subgroups for $group..."
-        read -r -a subgroups <<< `getSubgroups "$group"`
+        read -r -a subgroups <<< $(getSubgroups "$group")
         echo "Mirroring group $group with all subgroups: ${subgroups[@]} "
         for subgroup in ${subgroups[@]}; do
             include_path="${include_path},/${subgroup},/groups/${subgroup}"
@@ -215,14 +294,136 @@ function mirrorGroup () {
       echo "Mirroring group $group..."
     fi
 
-    $DRY_RUN $TOR wget --load-cookies "$COOKIE_FILE" --mirror \
+    $DRY_RUN $TOR "$WGET" $WGET_ARGS --load-cookies "$COOKIE_FILE" --mirror \
         --include "$include_path" --convert-links --retry-connrefused \
         --reject "edit.html" \
         --directory-prefix "${DOWNLOAD_DIR}" \
         --page-requisites --html-extension ${url}
 }
 
-function main () {
+# Function: scmRepositoryStatus
+#
+# Checks the download directory for the existence and status of the
+# source code management repository.
+#
+# Uses globals:
+#     $DOWNLOAD_DIR
+#     $SCM
+#
+# Returns 0 if the repository exists and is in an okay state.
+scmRepositoryStatus () {
+    local status_code
+    cd "$DOWNLOAD_DIR"
+    case "$SCM" in
+        git )
+            git status 2>/dev/null
+            status_code=$?
+            ;;
+    esac
+    cd -
+    return $status_code
+}
+
+# Function: scmInitializeRepository
+#
+# Creates a new repository using the given SCM backend in the
+# download directory.
+# Uses globals:
+#     $DOWNLOAD_DIR
+#     $SCM
+scmInitializeRepository () {
+    cd "$DOWNLOAD_DIR"
+    case "$SCM" in
+        git )
+            git init
+            ;;
+    esac
+    cd -
+}
+
+# Function: scmCommit
+#
+# Commits a new changeset/patch/version to the given SCM backend.
+#
+# Params:
+#     Any argument passed to this function will be passed to the SCM
+#     backend as part of its "commit" or "checkin" routine.
+#
+# Uses globals:
+#     $IFS
+#     $OLD_IFS
+#     $DOWNLOAD_DIR
+#     $COOKIE_FILE
+#     $SCM
+scmCommit () {
+    local string
+    local args
+
+    # Parse the string(s?) passed to this function as individual args.
+    string=$(echo "$@" | sed -Ee 's/ (-)?-/__NEWARG__\1-/g') # Use `__NEWARG__` as the delimeter.
+    # Convert the delimeter into a newline, then immediately treat that as an array in which the
+    # newlines are the only field separator (by setting $IFS explicitly).
+    IFS=$'\n'
+    args=(${string//__NEWARG__/$'\n'})
+
+    cd "$DOWNLOAD_DIR"
+    rm -f "$COOKIE_FILE" # Never commit cookies to version control.
+    case "$SCM" in
+        git )
+            git add .
+            git commit ${args[@]} # Pass the parsed arguments to the SCM backend.
+            ;;
+    esac
+    cd -
+
+    # Restore IFS.
+    IFS="$OLD_IFS"
+}
+
+# Function: checkPrerequisites
+#
+# Ensures we can actually proceed safely.
+# If we can't, issues an error code and exits.
+#
+# Uses globals:
+#     $PROGRAM
+#     $E_MISSING_WGET
+#     $SCM
+#     $E_MISSING_SCM
+#     $E_NONWRITABLE_DIR
+checkPrerequisites () {
+    # Check to make sure we have a `wget` available.
+    which -s wget
+    if [ 1 -eq $? ]; then
+        echo "FATAL: $PROGRAM cannot find a suitable 'wget' binary." >&2
+        exit $E_MISSING_WGET;
+    fi
+
+    which -s "$SCM"
+    if [ 1 -eq $? ]; then
+        echo "FATAL: $PROGRAM cannot find a suitable '$SCM' binary." >&2
+        exit $E_MISSING_SCM;
+    fi
+
+    # Warn on unsupported SCM backends.
+    local scms="cvs svn hz bzr"
+    for scm in scms; do
+        if [ x"$scm" == x"$SCM" ]; then
+            echo "WARNING: SCM backend '$1' is not yet implemented." >&2
+        fi
+    done;
+
+    # Check to ensure the download directory is writable.
+    if [ ! -w "$DOWNLOAD_DIR" ]; then
+        echo "FATAL: Permission denied to write in download directory '$DOWNLOAD_DIR'" >&2
+        exit $E_NONWRITABLE_DIR;
+    fi
+}
+
+# Function: main
+#
+# Do the thing!
+main () {
     # Process command line arguments.
     while test $# -gt 0; do
         if [ x"$1" == x"--" ]; then
@@ -230,7 +431,7 @@ function main () {
             shift
             break
         fi
-        case $1 in
+        case "$1" in
             --username | --user | -u )
                 shift
                 USERNAME="$1"
@@ -258,15 +459,59 @@ function main () {
                 TOR="torsocks"
                 ;;
 
+            --wget-args )
+                shift
+                WGET_ARGS="$1"
+                shift
+                ;;
+
             --download-directory )
                 shift
                 DOWNLOAD_DIR="$1"
                 shift
                 ;;
 
+            --quiet | -q )
+                WGET_ARGS="$WGET_ARGS --quiet"
+                shift
+                ;;
+
             --base-url )
                 shift
                 BASE_URL="$1"
+                shift
+                ;;
+
+            --scm )
+                shift
+                USE_SCM=1
+                case "$1" in
+                    cvs )
+                        SCM="$1"
+                        shift
+                        ;;
+                    svn )
+                        SCM="$1"
+                        shift
+                        ;;
+                    git )
+                        SCM="$1"
+                        shift
+                        ;;
+                    hg )
+                        SCM="$1"
+                        shift
+                        ;;
+                    bzr )
+                        SCM="$1"
+                        shift
+                        ;;
+                esac
+                ;;
+
+            --scm-args )
+                shift
+                SCM_ARGS="$1"
                 shift
                 ;;
 
@@ -281,8 +526,7 @@ function main () {
                 ;;
 
             -* )
-                echo "Unrecognized option: $1" >&2
-                usage
+                echo "FATAL: Unrecognized option: '$1'. Try '$PROGRAM --help' for help." >&2
                 exit $E_BAD_OPTION
                 ;;
 
@@ -292,9 +536,20 @@ function main () {
         esac
     done
 
+    # Collect runtime parameters.
+    if [ -z "$DOWNLOAD_DIR" ]; then
+        DOWNLOAD_DIR="$DEFAULT_DOWNLOAD_DIR"
+    fi
+    if [ -z "$SCM" ]; then
+        SCM="$DEFAULT_SCM"
+    fi
+
+    # Before we go any further, make sure we can, y'know, do things.
+    checkPrerequisites
+
     # We still need group names.
     if [ $# -lt 1 ]; then
-        usage
+        echo "FATAL: Missing group names. Try '$PROGRAM --help' for help." >&2
         exit $E_MISSING_ARG
     fi
 
@@ -305,9 +560,6 @@ function main () {
     if [ -z "$PASSWORD" ]; then
         read -s -p "Password: " PASSWORD
     fi
-    if [ -z "$DOWNLOAD_DIR" ]; then
-        DOWNLOAD_DIR="$DEFAULT_DOWNLOAD_DIR"
-    fi
     if [ -z "$BASE_URL" ]; then
         BASE_URL="$DEFAULT_BASE_URL"
     fi
@@ -317,6 +569,18 @@ function main () {
     for group in "$@"; do
         mirrorGroup "$group"
     done
+
+    if [ 1 -eq "$USE_SCM" ]; then
+        # Check to see if the download directory is already a repository.
+        scmRepositoryStatus
+        # If it is not, initialize a new repository there.
+        if [ 0 -ne $? ]; then
+            scmInitializeRepository
+        fi
+        # Once it is a repository, commit the newly downloaded mirrored
+        # contents using the given SCM arguments passed to us.
+        scmCommit $SCM_ARGS
+    fi
 }
 
 main "$@"
